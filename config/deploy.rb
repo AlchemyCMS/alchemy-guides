83af2df0ebcd3aa326188502d0dfb578207c13c5
Thomas von Deyen <tvd@bitspire.de>
2016-12-08 17:20:19 +0100

    Move Capfile and Gemfile to top level

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "alchemy-guides"
 
 set :scm, :none
-set :repository,  "./output"
+set :repository,  "./guides/output"
 set :deploy_via, :copy
 set :deploy_to, "/var/www/alchemy-guides"
 set :guide_version, -> do
@@ -11,8 +11,15 @@ end
 set :current_path, "#{deploy_to}/#{guide_version}"
 set :user, 'web1'
 
-role :app, "alchemy-cms.com"
+role :app, "109.234.107.117"
 set :use_sudo, false
+
+before 'deploy:update_code', :build_guides
+
+task :build_guides do
+  edge = fetch(:guide_version) == 'edge' ? true : false
+  run_locally "cd guides && bundle exec guides build #{edge ? '--edge --clean' : '--clean'}"
+end
 
 # Override default tasks which are not relevant to a non-rails app.
 namespace :deploy do
