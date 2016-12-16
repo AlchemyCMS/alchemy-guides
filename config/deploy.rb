5e4abd6f961078c5d194f0b130b6a7053f559ec8
Thomas von Deyen <tvd@bitspire.de>
2016-12-16 09:48:41 +0100

    Update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,15 +3,15 @@ set :application, "alchemy-guides"
 set :scm, :none
 set :repository,  "./guides/output"
 set :deploy_via, :copy
-set :deploy_to, "/var/www/alchemy-guides"
+set :deploy_to, "/var/www/alchemy-guides/html"
 set :guide_version, -> do
   branch = `git rev-parse --abbrev-ref HEAD`.strip
   branch == 'master' ? 'edge' : branch
 end
 set :current_path, "#{deploy_to}/#{guide_version}"
-set :user, 'web1'
+set :user, 'guides'
 
-role :app, "109.234.107.117"
+role :app, "alchemy-cms.com"
 set :use_sudo, false
 
 # Override default tasks which are not relevant to a non-rails app.
