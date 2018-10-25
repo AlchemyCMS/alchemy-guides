ea8a0f968e2fe33ce5950194c796b3b1860e1168
Thomas von Deyen <thomas@vondeyen.com>
2018-10-25 14:10:56 +0200

    Install vuepress

---
+++ guides/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'AlchemyCMS Guidelines',
+  description: 'Developer Guides for AlchemyCMS',
+  themeConfig: {
+    repo: 'AlchemyCMS/alchemy-guides',
+    editLinks: true,
+    docsDir: 'guides',
+    home: true,
+    nav: [
+      { text: 'Home', link: '/' }
+    ],
+    sidebar: 'auto'
+  }
+};
