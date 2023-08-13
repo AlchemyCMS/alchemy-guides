26765d2f74f5042cc5b17797e6c43323540eb90f
Thomas von Deyen <thomas@vondeyen.com>
2023-08-13 22:40:19 +0200

    Add Blish sponsor to page footer

---
+++ guides/.vuepress/layouts/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <ParentLayout>
+    <template #page-bottom>
+      <div class="footer">
+        AlchemyCMS is sponsored by
+        <a href="https://blish.cloud" target="_blank">
+          <img src="https://blish.cloud/BLISH_Logo.svg" alt="Blish Logo" />
+        </a>
+      </div>
+    </template>
+  </ParentLayout>
+</template>
+
+<script setup>
+  import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue"
+</script>
+
+<style lang="css" scoped>
+  .footer {
+    display: flex;
+    justify-content: center;
+  }
+
+  .footer a {
+    margin-left: 1ex;
+  }
+
+  .footer img {
+    height: 1rem;
+  }
+</style>
